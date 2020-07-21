39042ae6edc319ea2d52c191a814aa74f7339821
josherias <josherias10@gmail.com>
2020-07-21 18:00:42 +0300

    Updates

---
+++ precache-manifest.616753143a7854adf942414c68658694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcb8ee9a5370608c4eef02ac483b547",
+    "url": "/josherias/React-Todo-App/index.html"
+  },
+  {
+    "revision": "f62a05800f78cde8721a",
+    "url": "/josherias/React-Todo-App/static/css/main.d1f0a891.chunk.css"
+  },
+  {
+    "revision": "54afdfa744ae29664b7c",
+    "url": "/josherias/React-Todo-App/static/js/2.cc40d0fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/josherias/React-Todo-App/static/js/2.cc40d0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62a05800f78cde8721a",
+    "url": "/josherias/React-Todo-App/static/js/main.2e1e7ac0.chunk.js"
+  },
+  {
+    "revision": "a55d8cce8e012448e477",
+    "url": "/josherias/React-Todo-App/static/js/runtime-main.129b6b0a.js"
+  }
+]);
